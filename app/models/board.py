e54a9c2f65563aaeea989cc89b324cf989b203e3
mayboolean <>
2024-12-21 11:34:47 -0500

    establishes relationship bw boards to cards (one to many)

--- app/models/board.py
+++ app/models/board.py
@@ -1,7 +1,9 @@
 from ..db import db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     owner: Mapped[str]
+    # boards can have multiple cards
+    cards: Mapped[list["Card"]] = relationship(back_populates="board")
--- app/models/card.py
+++ app/models/card.py
@@ -1,7 +1,11 @@
 from ..db import db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import Optional
+from sqlalchemy import ForeignKey
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
     likes_count: Mapped[int]
+    board_id: Mapped[int] = mapped_column(ForeignKey('board_id'))
+    board: Mapped["Board"] = relationship(back_populates="cards")
