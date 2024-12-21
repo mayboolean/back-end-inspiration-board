9e4cd8fb759f9b7daa3961c4ea421655ef413327
Aleida Vieyra <avieyra@ufl.edu>
2024-12-21 12:40:40 -0500

    implemented relationships in board and card models

--- app/models/board.py
+++ app/models/board.py
@@ -1,7 +1,15 @@
 from ..db import db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .card import Card
 
 class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     owner: Mapped[str]
+
+    cards: Mapped[list["Card"]] = relationship(back_populates="board")
+
+    
--- app/models/card.py
+++ app/models/card.py
@@ -1,7 +1,16 @@
 from ..db import db
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .board import Board
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
     likes_count: Mapped[int]
+
+    board_id: Mapped[Optional[int]] = mapped_column(ForeignKey("board.board_id"))
+    board: Mapped[Optional["Board"]] = relationship(back_populates="cards")
