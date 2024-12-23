766395bbec90ca5b24bfe16a4e6bea3c33b69dc0
mayboolean <>
2024-12-23 12:29:50 -0500

    creates GET cards endpoint to get all cards

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -4,11 +4,26 @@ from .route_utilities import validate_model
 from ..models.card import Card
 
 '''
+GET /cards
 DELETE /cards/<card_id>
 PUT /cards/<card_id>/like
 '''
 
 bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
+
+@bp.get("")
+def get_all_cards():
+    '''
+    request: None -> list of dict of card objects
+    '''
+    query = db.select(Card).order_by(Card.id)
+    cards = db.session.scalars(query)
+
+    cards_response = []
+    for card in cards:
+        cards_response.append(card.to_dict())
+    return cards_response
+
 
 @bp.delete("/<card_id>")
 def delete_card(card_id):
