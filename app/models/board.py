026845aaf872117bc772d8c00f2786dbb14926a0
mayboolean <>
2024-12-22 21:32:26 -0500

    makes board class method from_dict and POST to create board

--- app/models/board.py
+++ app/models/board.py
@@ -9,9 +9,23 @@ class Board(db.Model):
     cards: Mapped[list["Card"]] = relationship(back_populates="board")
 
     def to_dict(self):
+        '''
+        board obj -> dict 
+        '''
         board_as_dict = {}
         board_as_dict["board_id"] = self.board_id
         board_as_dict["title"] = self.title
         board_as_dict["owner"] = self.owner
 
         return board_as_dict
+
+    @classmethod
+    def from_dict(cls, board_data):
+        '''
+        request_body (board_data) -> new instance of Board class with info
+        '''
+        new_board = cls(
+            title=board_data["title"],
+            owner=board_data["owner"]
+        )
+        return new_board
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, make_response
 from app.models.board import Board
 from ..db import db
 '''
@@ -12,6 +12,10 @@ bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 
 @bp.get("")
 def get_all_boards():
+    '''
+    request: None -> response: json of all boards
+    boards (db returns board object) which is turned into list of dict to return
+    '''
     query = db.select(Board).order_by(Board.board_id)
     boards = db.session.scalars(query)
 
@@ -19,3 +23,23 @@ def get_all_boards():
     for board in boards:
         boards_response.append(board.to_dict())
     return boards_response
+
+@bp.post("")
+def create_board():
+    '''
+    request: Board object -> response: Board obj (dict) and status code
+    takes request body, creates Board instance, adds+commits to db
+    '''
+    request_body = request.get_json()
+
+    try:
+        new_board = Board.from_dict(request_body)
+    
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+    
+    db.session.add(new_board)
+    db.session.commit()
+
+    return new_board.to_dict(), 200
