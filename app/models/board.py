a3349951476f73256dfd2c66cbe62443e5277c86
mayboolean <>
2024-12-22 20:28:10 -0500

    creates to_dict of Board class and get endpoint of board routes

--- app/models/board.py
+++ app/models/board.py
@@ -7,3 +7,11 @@ class Board(db.Model):
     owner: Mapped[str]
     # boards can have multiple cards
     cards: Mapped[list["Card"]] = relationship(back_populates="board")
+
+    def to_dict(self):
+        board_as_dict = {}
+        board_as_dict["board_id"] = self.board_id
+        board_as_dict["title"] = self.title
+        board_as_dict["owner"] = self.owner
+
+        return board_as_dict
--- app/routes/board_routes.py
+++ app/routes/board_routes.py
@@ -10,3 +10,12 @@ POST /boards/<board_id>/cards
 
 bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 
+@bp.get("")
+def get_all_boards():
+    query = db.select(Board).order_by(Board.board_id)
+    boards = db.session.scalars(query)
+
+    boards_response = []
+    for board in boards:
+        boards_response.append(board.to_dict())
+    return boards_response
