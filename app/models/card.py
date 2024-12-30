6d1fa72904338bc8d783df65fa151fd8cd3e478d
Aksana <adziamidava@gmail.com>
2024-12-30 15:20:09 -0800

    Changed board and card models

--- app/models/card.py
+++ app/models/card.py
@@ -1,13 +1,15 @@
-from app.models.board import Board
 from ..db import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional
 from sqlalchemy import ForeignKey
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .board import Board
 
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
-    likes_count: Mapped[int]
-    board_id: Mapped[int] = mapped_column(ForeignKey('board_id'))
+    likes_count: Mapped[Optional[int]] = mapped_column(default=0)
+    board_id: Mapped[int] = mapped_column(ForeignKey("board.board_id"))
     board: Mapped["Board"] = relationship(back_populates="cards")
