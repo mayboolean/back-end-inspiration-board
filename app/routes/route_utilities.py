87a320a2ea5343c60b33a705272b4267331ff14e
Aleida Vieyra <avieyra@ufl.edu>
2025-01-07 10:04:16 -0500

    changed validate_model invalid exception response to a 400, previously set to 404

--- app/routes/route_utilities.py
+++ app/routes/route_utilities.py
@@ -10,7 +10,7 @@ def validate_model(cls, model_id):
         model_id = int(model_id)
     except:
         response = {"message": f"{cls.__name__} {model_id} is invalid"}
-        abort(make_response(response, 404))
+        abort(make_response(response, 400))
     
     query = db.select(cls).where(cls.id == model_id)
     model = db.session.scalar(query)
